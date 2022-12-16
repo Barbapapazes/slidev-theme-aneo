ca20b10cf0b031d5a0c9a45da6047f80694a7819
Barbapapazes <e.soubiran25@gmail.com>
2022-12-16 11:58:40 +0100

    feat: create cover layout (#5)

--- example.md
+++ example.md
@@ -1,11 +1,23 @@
 ---
 theme: ./
+layout: cover
+image:
+  src: https://images.unsplash.com/photo-1535957998253-26ae1ef29506?fit=crop&w=736&q=80
+  alt: image alt text
+---
+
+# Titre de la présentation
+
+::subtitle::
+
+Octobre 2022
+
+---
 layout: section
 image:
   src: https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?fit=crop&w=1470&q=80
   alt: image alt text
 ---
-
 # Objectifs
 
 ---
---
+++ layouts/cover.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  image: {
+    src: string
+    alt: string
+  }
+}>()
+</script>
+
+<template>
+  <div class="slidev-layout cover flex flex-col justify-center items-center">
+    <div class="relative z-10">
+      <slot />
+      <div class="subtitle">
+        <slot name="subtitle" />
+      </div>
+    </div>
+    <div class="absolute inset-0">
+      <img :src="image.src" :alt="image.alt" class="object-cover w-full h-full" />
+    </div>
+    <div class="absolute inset-0 bg-black opacity-50" />
+    <img class="absolute bottom-8 left-8 h-12" src="/logos/aneo.png" alt="Logo d'Aneo">
+  </div>
+</template>
