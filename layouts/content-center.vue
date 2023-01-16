dc723534614afc2ea8970fd615b7150d4f5b9aff
Barbapapazes <e.soubiran25@gmail.com>
2023-01-16 17:40:51 +0100

    feat: create a content center layout (#151)

---
+++ layouts/content-center.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{
+  seeMore?: SeeMore
+}>()
+</script>
+
+<template>
+  <div class="slidev-layout content-center flex flex-col">
+    <SeeMore v-if="seeMore" :seeMore="seeMore" />
+    <slot />
+    <div class="flex-grow flex flex-col justify-center">
+      <slot name="center" />
+    </div>
+    <SlidesStatus></SlidesStatus>
+  </div>
+</template>
