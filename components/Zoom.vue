f34a8fa1a77cf325964e83215f42dcc9368f511c
Barbapapazes <e.soubiran25@gmail.com>
2023-03-10 09:01:48 +0100

    feat: add a zoom component for images

---
+++ components/Zoom.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+defineProps<{
+  src: string;
+  zoom?: string;
+  origin?: string;
+}>();
+
+</script>
+
+<template>
+  <div class="zoom overflow-hidden">
+    <img :src="src" />
+  </div>
+</template>
+
+<style>
+.zoom img {
+  transform: scale(v-bind(zoom));
+  transform-origin: v-bind(origin);
+}
+</style>
