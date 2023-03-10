39733fdfa3c4cf3443e0bf52789cbe71e94d0d9e
Barbapapazes <e.soubiran25@gmail.com>
2023-03-10 09:31:44 +0100

    feat: create section-content component (#202)

---
+++ components/SectionContent.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string
+  color: string
+}>()
+</script>
+
+<template>
+  <div class="section-content p-4 rounded-lg">
+    <div class="header font-bold">
+      {{ title }}
+    </div>
+    <div class="mt-2 content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+.section-content {
+  border: 1px solid v-bind(color);
+}
+
+.header {
+  color: v-bind(color);
+}
+</style>
